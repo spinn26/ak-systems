bdd85196f92af6747c3cca91852ef0ca9100ed43
ENCODE <Kaprielov93@gmail.com>
2026-04-18 11:32:44 +0300

    Analytics: enable webvisor, SSR, ecommerce dataLayer per Yandex Metrika counter

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -41,7 +41,14 @@ export function Analytics() {
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-        ym(${metrikaId}, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:false });
+        ym(${metrikaId}, "init", {
+          ssr: true,
+          webvisor: true,
+          clickmap: true,
+          trackLinks: true,
+          accurateTrackBounce: true,
+          ecommerce: "dataLayer"
+        });
       `}</Script>
       <noscript>
         <div>
